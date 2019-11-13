fbd491b655461674d26dc6596cd9473e42266284
minoruharikae <kamikayamaru5277@gmail.com>
2019-11-13 11:59:40 +0900

    htmlリファクタリング

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,56 +1,24 @@
 $(function(){
   var buildMessageHTML = function(message) {
-    if (message.content && message.image.url) {
-      //data-idが反映されるようにしている
-      var html = `<div class="message" data-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.date +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image.url + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    } else if (message.content) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.date +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
-    } else if (message.image.url) {
-      //同様に、data-idが反映されるようにしている
-      var html = `<div class="message" data-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
-          `</div>` +
-          `<div class="upper-message__date">` +
-            message.date +
-          `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image.url + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
-    };
+    var image = message.image.url ? `<img class= "lower-message__image" src= ${message.image.url} >` : "";
+    
+      var html = 
+      `<div class="message" data-id= ${message.id} >
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name} 
+          </div>
+          <div class="upper-message__date">
+            ${message.date} 
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content} 
+          </p>
+            ${image}
+        </div>
+      </div>`
 
     $('.messages').append(html);
     
